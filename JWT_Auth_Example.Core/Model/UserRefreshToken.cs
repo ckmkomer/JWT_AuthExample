8e964c939cc2a0ced891dbff80a3be7f9843dd25
ckmkomer <101353123+ckmkomer@users.noreply.github.com>
2024-05-05 13:07:30 +0300

    Add project files.

---
+++ JWT_Auth_Example.Core/Model/UserRefreshToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT_Auth_Example.Core.Model
+{
+	public class UserRefreshToken
+	{
+        public string UserId { get; set; }
+
+        public string Code { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
---
+++ JWT_Auth_Example.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT_Auth_Example.Core.Repositories
+{
+	public interface IGenericRepository<T> where T : class
+	{
+		Task<T> CreateAsync(T entity);
+		void Update(T entity);
+		void Delete(T entity);
+		Task<IEnumerable<T>> GetAllAsync();
+		Task<T> GetByIdAsync(int id);
+
+		IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+	}
+}
